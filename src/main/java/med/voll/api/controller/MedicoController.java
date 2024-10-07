37ab7f1b2d53e72385859e2dfe305c21081c3ba5
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2024-10-07 11:56:53 -0300

    =Usando o get fazendo paginação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,8 +1,9 @@
 package med.voll.api.controller;
 
-import java.util.List;
-
+import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,9 +30,8 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
     
-//    @GetMapping
-//    public List<DadosListagemMedico> listar() {
-//       // return repository.findAll().stream().map(DadosListagemMedico::new).toList();
-//    }
-
+    @GetMapping
+    public org.springframework.data.domain.Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
 }
