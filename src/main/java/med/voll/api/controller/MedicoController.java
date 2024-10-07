b58384fe692ee90e9fb8afd260116769b7fb5e37
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2024-10-07 10:59:49 -0300

    =cadastrando o medico realizando o metodo POST

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,20 +1,37 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import med.voll.api.modelo.DadosCadastroMedicos;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.modelo.DadosCadastroMedico;
+import med.voll.api.modelo.DadosListagemMedico;
+import med.voll.api.modelo.Medico;
+import med.voll.api.repository.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
-	
-	@PostMapping
-	public void cadastrar(@RequestBody DadosCadastroMedicos json) {
-		System.out.println(json);
-	}
-	
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
+    }
+    
+//    @GetMapping
+//    public List<DadosListagemMedico> listar() {
+//       // return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+//    }
 
 }
