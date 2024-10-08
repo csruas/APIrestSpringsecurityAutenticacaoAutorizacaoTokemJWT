24f58fa3437de8e5b93c798e1eb5891542a36647
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2024-10-08 09:12:32 -0300

    =Ultilizando o ResponseEntity para cada verbo HTTP do crud

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -29,43 +29,43 @@ import med.voll.api.repository.MedicoRepository;
 @RequestMapping("medicos")
 public class MedicoController {
 
-    @Autowired
-    private MedicoRepository repository;
+	@Autowired
+	private MedicoRepository repository;
 
-    @PostMapping
-    @Transactional
-    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
-    	var medico = new Medico(dados);
-    	repository.save(medico);
-        
-        var uri = uriBuilder.path("/medico/{id}").buildAndExpand(medico.getId()).toUri();
-        
-        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
-    }
-    
-    @GetMapping
-    public ResponseEntity<org.springframework.data.domain.Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        var page =  repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
-        
-        return ResponseEntity.ok(page);
-    }
-    
-    @PutMapping
-    @Transactional
-    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
-    	var medico = repository.getReferenceById(dados.id());
-    	medico.atualizarInformacoes(dados);
-    	
-    	return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
-    }
-    
-    @DeleteMapping("/{id}")
-    @Transactional
-    public ResponseEntity excluir(@PathVariable Long id) {
-    	var medico = repository.getReferenceById(id);
-    	medico.excluir();
-    	
-    	return ResponseEntity.noContent().build();
-    	
-    }
+	@PostMapping
+	@Transactional
+	public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+		var medico = new Medico(dados);
+		var uri = uriBuilder.path("/medico/{id}").buildAndExpand(medico.getId()).toUri();
+		repository.save(medico);
+
+		return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
+	}
+
+	@GetMapping
+	public ResponseEntity<org.springframework.data.domain.Page<DadosListagemMedico>> listar(
+			@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+		var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+
+		return ResponseEntity.ok(page);
+	}
+
+	@PutMapping
+	@Transactional
+	public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+		var medico = repository.getReferenceById(dados.id());
+		medico.atualizarInformacoes(dados);
+
+		return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity excluir(@PathVariable Long id) {
+		var medico = repository.getReferenceById(id);
+		medico.excluir();
+
+		return ResponseEntity.noContent().build();
+
+	}
 }
